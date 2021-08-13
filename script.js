a03e5ef74d98a078bf8ca254ceb6d28017a48cc1
Caio Lima <caiojlimah@gmail.com>
2021-08-13 16:05:39 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const input = document.getElementById('text-input');
 const pMemeText = document.getElementById('meme-text');
 const imgInsert = document.getElementById('meme-insert');
 const imgTag = document.getElementById('meme-image');
+const imgContainer = document.getElementById('meme-image-container');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
 
 function addTextMeme() {
   pMemeText.innerText = input.value;
@@ -15,3 +19,20 @@ function addImg(evn) {
 
 input.addEventListener('keyup', addTextMeme);
 imgInsert.addEventListener('change', addImg);
+
+function addBorder(e) {
+  const event = e;
+  if (event.target === fireButton) {
+    imgContainer.style.border = '3px red dashed';
+  }
+  if (event.target === waterButton) {
+    imgContainer.style.border = '5px blue double';
+  }
+  if (event.target === earthButton) {
+    imgContainer.style.border = '6px green groove';
+  }
+}
+
+fireButton.addEventListener('click', addBorder);
+waterButton.addEventListener('click', addBorder);
+earthButton.addEventListener('click', addBorder);
