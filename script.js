947fed595ca84ca9508e16f9f694e0bfcce43c81
Caio Lima <caiojlimah@gmail.com>
2021-08-13 15:48:06 -0300

    Requisito 2, 3, 4 e 5

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const input = document.getElementById('text-input');
 const pMemeText = document.getElementById('meme-text');
+const imgInsert = document.getElementById('meme-insert');
+const imgTag = document.getElementById('meme-image');
 
 function addTextMeme() {
   pMemeText.innerText = input.value;
 }
 
+function addImg(evn) {
+  const ev = evn;
+  const files = ev.target.files[0];
+  imgTag.src = URL.createObjectURL(files);
+}
+
 input.addEventListener('keyup', addTextMeme);
+imgInsert.addEventListener('change', addImg);
