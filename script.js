de9026779f46b1cdd2c87a6e0c48c65e1f19bfeb
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-15 09:47:41 -0300

    Requisito 3 feito

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-// const memeImageContainer = document.getElementById('meme-image-container');
+const memeImageContainer = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
 const textInput = document.getElementById('text-input');
+const imgFileInput = document.getElementById('meme-insert');
+const memeImage = document.getElementById('meme-image');
 
+// Type text
 textInput.addEventListener('input', () => {
   memeText.innerText = textInput.value;
+});
+
+// Add image file
+imgFileInput.addEventListener('change', (image) => {
+  const imageURL = URL.createObjectURL(image.target.files[0]);
+  memeImageContainer.style.backgroundImage = `url('${imageURL}')`;
 });
