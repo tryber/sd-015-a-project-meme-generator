6a4ef32003843fca6a3fb82047c43c4436e04320
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-16 18:44:03 -0300

    Requisito final

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// const memeImageContainer = document.getElementById('meme-image-container');
+const memeImageContainer = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
 const textInput = document.getElementById('text-input');
 const imgFileInput = document.getElementById('meme-insert');
 const memeImage = document.getElementById('meme-image');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
+const memeMiniList = document.getElementById('meme-miniatures').children;
 
 // Type text
 textInput.addEventListener('input', () => {
@@ -14,3 +18,23 @@ imgFileInput.addEventListener('change', (image) => {
   const imageURL = URL.createObjectURL(image.target.files[0]);
   memeImage.src = imageURL;
 });
+
+// Add three elemental buttons
+fireButton.addEventListener('click', () => {
+  memeImageContainer.style.border = '3px dashed red';
+});
+
+waterButton.addEventListener('click', () => {
+  memeImageContainer.style.border = '5px double blue';
+});
+
+earthButton.addEventListener('click', () => {
+  memeImageContainer.style.border = '6px groove green';
+});
+
+// Add meme images
+for (let i = 0; i < memeMiniList.length; i += 1) {
+  memeMiniList[i].addEventListener('click', (miniImg) => {
+    memeImage.src = miniImg.target.src;
+  });
+}
