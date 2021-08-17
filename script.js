3bc97aad3d535bf2e11d4fb5aeac7adb5e3bf068
alexandrexpj <alexandrexpjr@hotmail.com>
2021-08-17 18:26:11 -0300

    Requisitos 2 e 3

--- script.js
+++ script.js
@@ -2,12 +2,32 @@ window.addEventListener('load', function() {
     const getTextInput = document.getElementById('text-input');
     const getImageDiv = document.getElementById('meme-image-container');
     const getMemeSpan = document.getElementById('meme-text');
+    const getMemeImage = document.getElementById('meme-image');
+    const getImageInput = document.getElementById('meme-insert');
 
     getTextInput.addEventListener('keyup', function() {
         getMemeSpan.innerHTML = getTextInput.value;
-        // getTextInput.value = '';
     });
+
+    getImageInput.addEventListener('change', function(event) {
+        // document.getElementById('picField').onchange = function (evt) {
+        const files = event.target.files;
+
+        // FileReader support
+        if (FileReader && files && files.length) {
+            let fileReader = new FileReader();
+            fileReader.onload = function() {
+                getMemeImage.style.backgroundImage = `url(${fileReader.result})`;
+            }
+            fileReader.readAsDataURL(files[0]);
+        }  
+    });
+    // Parte do fileReader retirada do stackOverflow
+    // Link: https://stackoverflow.com/questions/3814231/loading-an-image-to-a-img-from-input-file
+
 
 
 
 });
+
+console.log(360 * 2.68055555);
