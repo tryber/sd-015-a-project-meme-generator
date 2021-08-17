58f22c819871965470b7e76c0c703678604a085f
Leandro <Playertox1c@gmail.com>
2021-08-17 15:58:13 -0300

    lint js

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
 //  Escreve o texto na imagem
 function write() {
-  const input = document.getElementById('text-input')
-  const h1 = document.getElementById('meme-text')
+  const input = document.getElementById('text-input');
+  const h1 = document.getElementById('meme-text');
   input.addEventListener('keyup', (e) => {
-    let valor = e.target.value
-    h1.innerHTML = valor
-  })
+    const valor = e.target.value;
+    h1.innerHTML = valor;
+  });
 }
-write()
+write();
 
 //  Escolhendo imagem do meme
 function getImage() {
-  const file = document.getElementById('meme-insert')
-  const image = document.getElementById('meme-image')
+  const file = document.getElementById('meme-insert');
+  const image = document.getElementById('meme-image');
   file.addEventListener('change', (_) => {
-    for(let index of file.files) {
-      const valor = URL.createObjectURL(index)
-      image.src = valor
+    for (let c = 0; c < file.files.length; c += 1) {
+      const valor = URL.createObjectURL(file.files[c]);
+      image.src = valor;
     }
-  })
+  });
 }
-getImage()
+getImage();
 
 //  Escolhendo a borda
 function getBorder() {
-  const botContainer = document.getElementById('bot-container')
-  const image = document.getElementById('meme-image-container')
-  botContainer.addEventListener('click', e => {
-    let classe = e.target.className.split(' ')[1]
-    image.className = classe
-  })
+  const botContainer = document.getElementById('bot-container');
+  const image = document.getElementById('meme-image-container');
+  botContainer.addEventListener('click', (e) => {
+    const classe = e.target.className.split(' ')[1];
+    image.className = classe;
+  });
 }
-getBorder()
+getBorder();
 
 //  Escolhendo templates
 function getTemplates() {
-  const imgContainer = document.getElementById("imagens-container")
-  const image = document.getElementById('meme-image')
-  imgContainer.addEventListener('click', e => {
-    let value = e.target.src
-    value !== '' ? image.src = value : ''
-
-  })
+  const imgContainer = document.getElementById('imagens-container');
+  const image = document.getElementById('meme-image');
+  imgContainer.addEventListener('click', (e) => {
+    const value = e.target.src;
+    if (value !== '') image.src = value;
+  });
 }
-getTemplates()
+getTemplates();
