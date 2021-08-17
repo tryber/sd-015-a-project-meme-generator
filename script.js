34bcc9b416f7a5bc13ede8c2968dab382d516b79
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-08-17 20:22:27 -0300

    Lint adjustments

--- script.js
+++ script.js
@@ -5,36 +5,36 @@ const displayImg = document.getElementById('meme-image');
 const fireButton = document.getElementById('fire');
 const waterButton = document.getElementById('water');
 const earthButton = document.getElementById('earth');
-const memeContainer = document.getElementById('meme-image-container')
+const memeContainer = document.getElementById('meme-image-container');
+
+function displayMemeText() {
+  displayText.innerHTML = inputText.value;
+}
+
+function displayImage() {
+  displayImg.innerText = uploadImg.value;
+}
+
+function borderStyle(event) {
+  if (event.target === fireButton) {
+    memeContainer.style.borderStyle = 'dashed';
+    memeContainer.style.borderWidth = '3px';
+    memeContainer.style.borderColor = 'red';
+  }
+  if (event.target === waterButton) {
+    memeContainer.style.borderStyle = 'double';
+    memeContainer.style.borderWidth = '5px';
+    memeContainer.style.borderColor = 'blue';
+  }
+  if (event.target === earthButton) {
+    memeContainer.style.borderStyle = 'groove';
+    memeContainer.style.borderWidth = '6px';
+    memeContainer.style.borderColor = 'green';
+  }
+}
 
 inputText.addEventListener('keyup', displayMemeText);
 fireButton.addEventListener('click', borderStyle);
 waterButton.addEventListener('click', borderStyle);
 earthButton.addEventListener('click', borderStyle);
 uploadImg.addEventListener('change', displayImage);
-
-function displayMemeText() {
-    displayText.innerHTML = inputText.value;
-}
-
-function displayImage() {
-    displayImg.innerText = uploadImg.value;
-}
-
-function borderStyle(event) {
-    if(event.target === fireButton) {
-       memeContainer.style.borderStyle = 'dashed';
-       memeContainer.style.borderWidth = '3px';
-       memeContainer.style.borderColor = 'red';
-    }
-    if(event.target === waterButton) {
-       memeContainer.style.borderStyle = 'double';
-       memeContainer.style.borderWidth = '5px';
-       memeContainer.style.borderColor = 'blue';
-    }
-    if(event.target === earthButton) {
-       memeContainer.style.borderStyle = 'groove';
-       memeContainer.style.borderWidth = '6px';
-       memeContainer.style.borderColor = 'green';
-    }
-}
