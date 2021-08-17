c216180cba5b102b8b4ccff62ba48e00493df76c
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-17 17:44:56 -0400

    Resolve requisito 2 do projeto bonus meme-generator-bloco 5. Rodando perfeitamente no npm test local.

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const getInput = document.getElementById('text-input');
-getInput.addEventListener('keyup', printText);
-
+// Requisito 1 - Crie uma caixa de texto com a qual quem usa pode interagir para inserir texto em cima da imagem escolhi.
 function printText() {
-  const getText = document.getElementById('text');
+  const getText = document.getElementById('meme-text');
   getText.innerHTML = '';
   getText.innerHTML = getInput.value;
 }
+
+const getInput = document.getElementById('text-input');
+getInput.addEventListener('keyup', printText);
+
+// Requisito 2 - O site deve permitir que quem usa faça upload de uma imagem de seu computador.
+function loadFile(event) {
+  imgOutput = document.getElementById('meme-image');
+  imgOutput.src = URL.createObjectURL(event.target.files[0])
+  // window.URL.revokeObjectURL(imgOutput);
+}
+
+
+const getMemeInsert = document.getElementById('meme-insert');
+getMemeInsert.addEventListener('change', loadFile);
