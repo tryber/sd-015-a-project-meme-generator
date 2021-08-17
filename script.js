36750cd76aece1e4702ae08ba1271b82030e3a2b
lucas-morais <lucasmoraisss@gmail.com>
2021-08-17 18:03:21 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function showText() {
   const input = document.getElementById('text-input');
   const textContainer = document.getElementById('meme-text');
-
+  const paragraph = document.createElement('p');
   input.addEventListener('keyup', (event) => {
-    textContainer.innerText = event.target.value;
+    paragraph.innerText = event.target.value;
+    textContainer.appendChild(paragraph);
   });
 }
 
 showText();
+
+function showImage() {
+  const upload = document.getElementById('meme-insert');
+  const imageDiv = document.getElementById('meme-image');
+  upload.addEventListener('change', () => {
+    const image = document.createElement('img');
+    image.src = URL.createObjectURL(upload.files[0]);
+    image.id = 'meme';
+    imageDiv.appendChild(image);
+  });
+}
+
+showImage();
