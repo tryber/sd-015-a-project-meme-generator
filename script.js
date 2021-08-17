feb7273109b43ff49f422e86ba7a1d0f6e855a1d
Kelvin Pichinini <kelvinpichinini@gmail.com>
2021-08-17 17:43:38 -0300

    requisito 6 (bordas)

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const textInput = document.getElementById('text-input');
 const memeTxt = document.getElementById('meme-text');
 const uploadInput = document.getElementById('meme-insert');
 const memeImg = document.getElementById('meme-image');
+const fireBttn = document.getElementById('fire');
+const waterBttn = document.getElementById('water');
+const earthBttn = document.getElementById('earth');
 
 function addText() {
   textInput.addEventListener('keyup', (event) => {
     memeTxt.innerHTML = event.target.value;
   });
 }
+
 function uploadImage() {
   uploadInput.addEventListener('change', (event) => {
     const fileURL = window.URL.createObjectURL(event.target.files[0]);
@@ -19,6 +23,16 @@ function uploadImage() {
   });
 }
 
+function changeBorder(event) {
+  memeContainer.style.border = getComputedStyle(event.target).border;
+}
+
+function applyBorder() {
+  fireBttn.addEventListener('click', changeBorder);
+  waterBttn.addEventListener('click', changeBorder);
+  earthBttn.addEventListener('click', changeBorder);
+}
 
 addText();
 uploadImage();
+applyBorder();
