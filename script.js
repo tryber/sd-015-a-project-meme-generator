db5a0bb99d753ec85c5c13e0024243baad066824
Kelvin Pichinini <kelvinpichinini@gmail.com>
2021-08-17 16:46:31 -0300

    requisitos 1,2 e 3

---
+++ script.js
@@ -0,0 +1,23 @@
+const memeContainer = document.getElementById('meme-image-container');
+const textInput = document.getElementById('text-input');
+const memeTxt = document.getElementById('meme-text');
+const uploadInput = document.getElementById('meme-insert');
+const memeImg = document.getElementById('meme-image');
+
+function addText() {
+  textInput.addEventListener('keyup', (event) => {
+    memeTxt.innerHTML = event.target.value;
+  });
+}
+function uploadImage() {
+  uploadInput.addEventListener('click', (event) => {
+    const fileURL = window.URL.createObjectURL(event.target.files[0]);
+    memeImg.src = fileURL;
+    const style = getComputedStyle(memeContainer);
+    memeImg.style.height = style.height;
+    memeImg.style.width = style.width;
+  });
+}
+
+addText();
+uploadImage();
