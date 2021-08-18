d0f18140684fbeff93d947c9e780932fe6c4c569
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-17 21:14:30 -0400

    Resolve requisito 4 do projeto meme-generator- bloco 5. Rodando perfeitamente no npm test local.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Requisito 1 - Crie uma caixa de texto com a qual quem usa pode interagir para inserir texto em cima da imagem escolhi.
+const getInput = document.getElementById('text-input');
+
 function printText() {
   const getText = document.getElementById('meme-text');
   getText.innerHTML = '';
   getText.innerHTML = getInput.value;
 }
 
-const getInput = document.getElementById('text-input');
 getInput.addEventListener('keyup', printText);
 
 // Requisito 2 - O site deve permitir que quem usa faça upload de uma imagem de seu computador.
@@ -15,9 +16,10 @@ function loadFile(event) {
   // window.URL.revokeObjectURL(imgOutput);
 }
 
-
 const getMemeInsert = document.getElementById('meme-insert');
 getMemeInsert.addEventListener('change', loadFile);
 
-// Requisito 3 - Adicione uma moldura no container. A moldura deve ter 1 pixel de largura, deve ser preta e do tipo 'solid'. A área onde a imagem aparecerá deve ter fundo branco.
+// Requisito 3 --> RESOLVIDO PELO CSS <-- Adicione uma moldura no container. A moldura deve ter 1 pixel de largura, deve ser preta e do tipo 'solid'. A área onde a imagem aparecerá deve ter fundo branco. --> RESOLVIDO PELO CSS <--
+
+// Requisito 4 - Adicione o texto que será inserido sobre a imagem deve ter uma cor, sombra e tamanho específicos.
 
