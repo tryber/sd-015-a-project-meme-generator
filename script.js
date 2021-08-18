7973e118b9cf8a576c2c7d5f93f70ec732253ec0
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-17 20:58:16 -0400

    Resolve requisito 3 do projeto meme-generator- bloco 5. Rodando perfeitamente no teste local pelo npm test.

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ getInput.addEventListener('keyup', printText);
 // Requisito 2 - O site deve permitir que quem usa faça upload de uma imagem de seu computador.
 function loadFile(event) {
   imgOutput = document.getElementById('meme-image');
-  imgOutput.src = URL.createObjectURL(event.target.files[0])
+  imgOutput.src = URL.createObjectURL(event.target.files[0]);
   // window.URL.revokeObjectURL(imgOutput);
 }
 
 
 const getMemeInsert = document.getElementById('meme-insert');
 getMemeInsert.addEventListener('change', loadFile);
+
+// Requisito 3 - Adicione uma moldura no container. A moldura deve ter 1 pixel de largura, deve ser preta e do tipo 'solid'. A área onde a imagem aparecerá deve ter fundo branco.
+
