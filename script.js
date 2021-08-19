416d79f0ceb449ccf2c30bce3bd04868bafb2631
Augusto Alves <augusto.malves@yahoo.com.br>
2021-08-18 23:25:08 -0300

    completo!

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const earth = document.getElementById('earth');
 const imgContainer = document.getElementById('meme-image-container');
 const imgPlace = document.getElementById('meme-image');
 const memeText = document.getElementById('meme-text');
-const memesProntos = document.getElementsByClassName('meme-pronto');
+const memesProntos = document.getElementById('memes-prontos');
 
 function subirFoto(event) {
   imgPlace.src = URL.createObjectURL(event.target.files[0]);
@@ -18,3 +18,25 @@ function mudaTexto() {
 }
 
 inputText.addEventListener('keyup', mudaTexto);
+
+function tacaFogo() {
+  imgContainer.style.border = '3px dashed red';
+  console.log('ta pegando fogo bixo!');
+}
+
+function tacaAgua() {
+  imgContainer.style.border = '5px double blue';
+  console.log('Taca Agua nele ali fera!');
+}
+
+function tacaTerra() {
+  imgContainer.style.border = '6px groove green';
+  console.log('essa fera ai meu!');
+}
+function addMeme(event) {
+  imgPlace.src = event.target.src;
+}
+earth.addEventListener('click', tacaTerra);
+water.addEventListener('click', tacaAgua);
+fire.addEventListener('click', tacaFogo);
+memesProntos.addEventListener('click', addMeme);
