3b200c24cba1ddf7ba5ad1ee36a48aeb52a1945c
Guarize <pedroguarize@hotmail.com>
2021-08-29 22:00:52 -0300

    Requirement - 6

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const input = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const uploadImg = document.getElementById('meme-insert');
 const img = document.getElementById('meme-image');
+const imgContainer = document.getElementById('meme-image-container');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
 
 input.addEventListener('keyup', () => {
   memeText.innerText = input.value;
@@ -10,4 +14,16 @@ input.addEventListener('keyup', () => {
 // Referencia para 'URL.createObjectURL' https://stackoverflow.com/questions/11708797/inserting-a-file-input-as-an-img-in-the-dom
 uploadImg.addEventListener('change', () => {
   img.src = URL.createObjectURL(uploadImg.files[0]);
+});
+
+fire.addEventListener('click', () => {
+  imgContainer.style.border = '3px dashed red';
+});
+
+water.addEventListener('click', () => {
+  imgContainer.style.border = '5px double blue';
+});
+
+earth.addEventListener('click', () => {
+  imgContainer.style.border = '6px groove green';
 });
