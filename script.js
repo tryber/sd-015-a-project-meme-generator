7cd06657feecc228cf0bf0d500f94711b8fc8f18
Guarize <pedroguarize@hotmail.com>
2021-08-29 21:46:52 -0300

    Requirement - 2

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const input = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const uploadImg = document.getElementById('meme-insert');
+const img = document.getElementById('meme-image');
 
 input.addEventListener('keyup', () => {
   memeText.innerText = input.value;
+});
+
+// Referencia para 'URL.createObjectURL' https://stackoverflow.com/questions/11708797/inserting-a-file-input-as-an-img-in-the-dom
+uploadImg.addEventListener('change', () => {
+  img.src = URL.createObjectURL(uploadImg.files[0]);
 });
