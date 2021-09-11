fb2d6de17b12aa405e0849a5628b820fe7803aff
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-09-11 19:35:40 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const placeImg = document.getElementById('meme-image-container');
+
 const sendText = () => {
   const textInput = document.getElementById('text-input').value;
   const textMeme = document.getElementById('meme-text');
@@ -9,7 +11,6 @@ typeText.addEventListener('keyup', sendText);
 
 const sendImage = () => {
   const imageInput = document.getElementById('meme-insert');
-  const placeImg = document.getElementById('meme-image-container');
 
   const image = document.createElement('img');
   image.src = URL.createObjectURL(imageInput.files[0]);
@@ -19,3 +20,31 @@ const sendImage = () => {
 
 const chosenImg = document.getElementById('meme-insert');
 chosenImg.addEventListener('change', sendImage);
+
+const fireBorder = () => {
+  placeImg.style.border = 'red dashed 3px';
+};
+
+const fire = document.getElementById('fire');
+fire.addEventListener('click', fireBorder);
+
+const waterBorder = () => {
+  placeImg.style.border = 'blue double 5px';
+};
+
+const water = document.getElementById('water');
+water.addEventListener('click', waterBorder);
+
+const earthBorder = () => {
+  placeImg.style.border = 'green groove 6px';
+};
+
+const earth = document.getElementById('earth');
+earth.addEventListener('click', earthBorder);
+
+const simpleBorder = () => {
+  placeImg.style.border = 'black solid 1px';
+};
+
+const simple = document.getElementById('simple');
+simple.addEventListener('click', simpleBorder);
