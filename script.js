4171fbbee0facce229519c04123a6bdecdb069ab
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-09-11 02:13:59 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const sendText = () => {
 
 const typeText = document.getElementById('text-input');
 typeText.addEventListener('keyup', sendText);
+
+const sendImage = () => {
+  const imageInput = document.getElementById('meme-insert');
+  const placeImg = document.getElementById('meme-image-container');
+
+  const image = document.createElement('img');
+  image.src = URL.createObjectURL(imageInput.files[0]);
+  image.id = 'meme-image';
+  placeImg.appendChild(image);
+};
+
+const chosenImg = document.getElementById('meme-insert');
+chosenImg.addEventListener('change', sendImage);
