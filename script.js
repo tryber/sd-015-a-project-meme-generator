eca40a9a0289f1b387c846fbc41eb27fabe4d5ae
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-21 21:09:06 -0300

    Teste de cypress

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
-const inputImage = document.getElementById('meme-insert')
-const memeImage = document.getElementById('meme-image')
+const inputImage = document.getElementById('meme-insert');
+const memeImage = document.getElementById('meme-image');
+const container = document.getElementById('meme-image-container')
 
 function escreveMeme() {
   memeText.innerText = inputText.value;
@@ -9,7 +10,9 @@ function escreveMeme() {
 
 function desenhaImagem() {
   memeImage.src = URL.createObjectURL(inputImage.files[0]);
+  container.style.height = memeImage.style.height;
+  container.width = memeImage.width;
 }
 
 inputText.addEventListener('keyup', escreveMeme);
-inputImage.addEventListener('input', desenhaImagem)
+inputImage.addEventListener('input', desenhaImagem);
