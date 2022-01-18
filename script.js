87f7a2e3532588ab3e19c4eb53f38accfb95925c
Arthur <arthurteixeira0805@gmail.com>
2022-01-18 20:02:00 -0300

    ✨ feat: adding req 6

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const upload = document.getElementById('meme-insert');
 const image = document.getElementById('meme-image');
-// const container = document.getElementById('meme-image-container');
+const container = document.getElementById('meme-image-container');
+const 
 
 textInput.addEventListener('keyup', () => {
   memeText.innerText = textInput.value;
@@ -11,3 +12,10 @@ textInput.addEventListener('keyup', () => {
 upload.addEventListener('change', () => {
   image.src = URL.createObjectURL(upload.files[0]);
 });
+
+ container.classList.add('fire');
+ container.classList.remove('water', 'earth');
+ container.classList.add('water');
+ container.classList.remove('fire', 'earth');
+ container.classList.add('earth');
+ container.classList.remove('water', 'fire');
